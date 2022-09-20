c3e1f82b9721388b13caef2473c9dc41c36736e6
kezzhou <102555416+kezzhou@users.noreply.github.com>
2022-09-20 16:00:54 -0400

    Updated 2
    Finished updating script and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # descriptives-nydischarges
 HHA 507 // Week 4 // Assignment 4 // Part 3
+
+## in this repo, we implement lessons from Part 1 and Part 2, found at: https://github.com/kezzhou/descriptives-scipy and https://github.com/kezzhou/descriptives-realpython/blob/main/scripts/realpython.py respectively. 
+
+## the dataset that this repo is centered around is the familiar SPARCS 2016 dataset found at https://health.data.ny.gov/Health/Hospital-Inpatient-Discharges-SPARCS-De-Identified/gnzp-ekau
+
+## The repo is notably split into three sections. The first section organizes columns by categorical and numerical data into a TableOne table, which conveniently and cleanly displays data that can be exported into a csv
+
+## The second section focuses on drawing descriptive statistics into a readable and easily understable format from the dataset for potential clients. We utilize multiple imports such as (but not limited to) pandas and numpy.
+
+## the final section focuses on simple visualization as an aid to supplement our statistical data. This section focuses on matplotlib.
+
+## All csvs are not pushed with the repo to avoid GitHub committing issues based on size limits
--- scripts/nydischarges.py
+++ scripts/nydischarges.py
@@ -1,9 +1,17 @@
 #### Imports ####
 
 import pandas as pd
+
 import researchpy as rp
+
 import tableone as t1
 from tableone import TableOne, load_dataset
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import pandas.plotting as pdp
 
 #### Dataset Pruning ####
 
@@ -11,21 +19,21 @@ from tableone import TableOne, load_dataset
 
 df = pd.read_csv('data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2016.csv')
 
-newdf = df.drop(['Facility Id', 'Length of Stay'], axis=1)
-
-newdf = df.drop(df.loc[339:].index, inplace=True)
+newdf = df.drop(df.loc[339:].index, inplace=True) ## drops rows beyond row 339. all rows retain their original row number
 
 newdf = df.dropna(axis=0) ## this will drop rows with nan values
 
 newdf.shape
 
-newdf.to_csv('data/new_SPARCS.csv')
+newdf.dtypes
+
+newdf.to_csv('data/new_SPARCS.csv') ## setting the new csv into stone
 
 
 
 #### Building a table with TableOne ####
 
-my_data = pd.read_csv('data/new_SPARCS.csv')
+my_data = pd.read_csv('data/new_SPARCS.csv') 
 
 df = my_data.copy()
 
@@ -35,30 +43,86 @@ df.columns
 
 df.head(5)
 
-df_columns = ['Age', 'HR', 'Group', 'sBP', 'Smoke']
+df_columns = ['Length of Stay', 'Gender', 'Ethnicity']
 
-df_categories = ['Smoke', 'Group']
+df_categories = ['Gender', 'Ethnicity']
 
-df_groupby = ['Smoke']
+df_groupby = ['Emergency Department Indicator']
 
-# df2['Vocation'].value_counts()
+df_table1 = TableOne(df, columns=df_columns, 
+    categorical=df_categories, groupby=df_groupby, pval=False)
 
-df2_table1 = TableOne(df2, columns=df2_columns, 
-    categorical=df2_categories, groupby=df2_groupby, pval=False)
+print(df_table1.tabulate(tablefmt = "fancy_grid"))
 
-print(df2_table1.tabulate(tablefmt = "fancy_grid"))
+df_table1.to_csv('data/tableone.csv')
 
-df2_table1.to_csv('descriptive/example1/data/test2.csv')
+
 
 
 #### Data Analysis with ResearchPy ####
 
-df = pd.read_csv('data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2016.csv')
+df = pd.read_csv('data/new_SPARCS.csv')
 
-rp.codebook(df)
+rp.codebook(df) ## works very well with categorical data
 
 df.columns
 
-rp.summary_cont(df[['Total Charges', 'Total Costs']])
+df.dtypes
 
-rp.summary_cat(df[[]])
+rp.summary_cont(df[['Length of Stay', 'Birth Weight']])
+
+rp.summary_cat(df[['Health Service Area', 'Hospital County', 'Emergency Department Indicator', 'Abortion Edit Indicator', 'Payment Typology 1', 'Payment Typology 2', 'Payment Typology 3', 'APR Medical Surgical Description', 'APR Risk of Mortality', 'APR Severity of Illness Description', 'APR Severity of Illness Code', 'APR MDC Description', 'APR MDC Code', 'APR DRG Description', 'APR DRG Code', 'CCS Procedure Description']])
+
+df.describe() ## to get a sweeping descriptive stat summary we can use .describe()
+## however, not all of these are relevant. even though Operating Certificate Number is int type, it makes no sense to compare them like this
+
+
+## let's say we want to look at the average Length of Stay by an individual's Gender
+
+groupby_gender = df.groupby('Gender')
+for gender, value in groupby_gender['Length of Stay']:
+    print(
+        gender, np.log(value.mean())
+        )
+
+## the average stay for Females is 1.2 days and the average stay for Males is 1.4 days
+
+#### Visualization ####
+
+## let's sprinkle in some visualization for the nurses
+
+## we can start with a pie chart on Gender
+
+df['Gender'].value_counts()
+
+x, y = 182, 118
+
+fig, ax = plt.subplots()
+
+ax.pie((x, y), labels=('F', 'M'), autopct='%1.1f%%')
+
+plt.show()
+
+## now let's do a bar chart for Age Group
+
+df['Age Group'].value_counts()
+
+x = ['0 to 17', '18 to 29', '30 to 49', '50 to 69', '70 or Older']
+
+h = [58, 50, 38, 64, 90]
+
+plt.barh(x, h)
+
+plt.xlabel('Age Group')
+
+plt.ylabel('Patient Count')
+
+plt.title('Patient Count by Age Group')
+
+plt.show()
+
+
+## Scatter matrices 
+
+pdp.scatter_matrix(
+    df[['Length of Stay', 'Birth Weight']] ## unfortunately the dataset leaves much to be desired for by way of numerical data
